df728e564da34bdc13e5410514ad9f92b054024f
rohlig <47533685+rohlig@users.noreply.github.com>
2020-03-10 20:51:55 +0100

    Revert "Revert "Projektdateien hinzufügen.""
    This reverts commit 75c349272bb61fca58d4a16cde74e0e0ce856d0e.

---
+++ GemeindeJubilaen/Services/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace GemeindeJubiläen.Models
+{
+    public class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            //Establishing the conection
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Item>().Wait();
+        }
+
+        // Show the registers
+        public Task<List<Item>> GetPeopleAsync()
+        {
+            return _database.Table<Item>().ToListAsync();
+        }
+
+        // Save registers
+        public Task<int> SavePersonAsync(Item contact)
+        {
+            return _database.InsertAsync(contact);
+        }
+
+        // Delete registers
+        public Task<int> DeletePersonAsync(Item contact)
+        {
+            return _database.DeleteAsync(contact);
+        }
+
+        // Save registers
+        public Task<int> UpdatePersonAsync(Item contact)
+        {
+            return _database.UpdateAsync(contact);
+        }
+
+
+    }
+}
---
+++ GemeindeJubilaen/ViewModels/AboutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace GemeindeJubiläen.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public String Pfad { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public AboutViewModel()
+        {
+            Title = "Import/Export";
+        }
+    }
+}
